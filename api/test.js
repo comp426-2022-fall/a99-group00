1f47add4bfcc5b831f8cf16a9f5182a3512e393e
aw811502 <77753326+aw811502@users.noreply.github.com>
2022-12-06 20:53:35 -0500

    deleted unnecessary files

--- api/test.js
+++
@@ -1,5 +0,0 @@
-import {execute} from './lib/db.js';
-const user = "Alan3";
-const query = `DELETE FROM users WHERE users.username = '${user}'`;
-const result = await execute(query);
-console.log(result);
