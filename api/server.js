ea57bb5e8c9efbe21854b938f6ca25fa9ac8a1e9
aw811502 <77753326+aw811502@users.noreply.github.com>
2022-12-06 22:56:48 -0500

    Updated endpoint output behavior

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ app.get('/app/', (req, res) => {
 // GET: get specified user info
 app.get('/app/users/:username', async(req, res) => {
     const query = `SELECT * FROM users WHERE username='${req.params.username}'`;
-    const result = await execute(query);
+    const result = await execute_get(query);
 
     const output = {
         exists: false,
@@ -35,36 +35,90 @@ app.get('/app/users/:username', async(req, res) => {
 
 // POST: create new user with specified username
 app.post('/app/users/:username', async(req, res) => {
-    const query = `INSERT INTO users(username) VALUES ('${req.params.username}')`;
-    const result = await execute(query);
-    res.status(200).send(result);
+    const username = req.params.username;
+
+    // First check if username already exists
+    const query_check = `SELECT * FROM users WHERE username='${username}'`;
+    const result_check = await execute(query_check);
+    if (result_check.length > 0) {
+        const output = {
+            status: 400,
+            message: `${username} already exists`
+        }
+        res.status(400).send(output);
+    }
+
+    // If user does not exist, create user
+    const query = `INSERT INTO users(username) VALUES ('${username}')`;
+    const result = await execute(query)
+    const output = {
+        status: 200,
+        message: `${username} was successfully created`
+    }
+    res.status(200).send(output);
 });
 
 // PATCH: update score/last access date of user
 app.patch('/app/users/:username/correct', async(req, res) => {
     const query = `UPDATE users SET number_attempts=number_attempts+1, number_correct=number_correct+1 WHERE username = '${req.params.username}'`;
     const result = await execute(query);
-    res.status(200).send(result);
+
+    const output = {
+        status: 400,
+        message: "${Username} does not exist."
+    }
+    if (result.affectedRows == 1) {
+        output.status = 200;
+        output.message = "${Username} was succesfully updated.";
+        res.status(200).send(output);
+    } else {
+        res.status(400).send(output);
+    }
 });
 
 app.patch('/app/users/:username/incorrect', async(req, res) => {
     const query = `UPDATE users SET number_attempts=number_attempts+1 WHERE username = '${req.params.username}'`;
     const result = await execute(query);
-    res.status(200).send(result);
+
+    const output = {
+        status: 400,
+        message: "${Username} does not exist."
+    }
+    if (result.affectedRows == 1) {
+        output.status = 200;
+        output.message = "${Username} was succesfully updated.";
+        res.status(200).send(output);
+    } else {
+        res.status(400).send(output);
+    }
 });
 
 // DELETE: delete user from database
 app.delete('/app/users/:username', async(req, res) => {
     const query = `DELETE FROM users WHERE users.username = '${req.params.username}'`;
     const result = await execute(query);
-    res.status(200).send(result);
+
+    const output = {
+        status: 400,
+        message: "${Username} does not exist."
+    }
+    if (result.affectedRows == 1) {
+        output.status = 200;
+        output.message = "${Username} was succesfully deleted.";
+        res.status(200).send(output);
+    } else {
+        res.status(400).send(output);
+    }
 });
 
 // GET: get 5 top scoring users
 app.get('/app/leaderboard', async(req, res) => {
     const query = "SELECT * FROM users ORDER BY number_correct DESC LIMIT 5";
     const result = await execute(query);
-    res.status(200).send(result);
+    const output = {
+        users: result
+    }
+    res.status(200).send(output);
 });
 
 // Undefined paths
