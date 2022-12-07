930bd9d001a7707f6c707afa8c187219ec313f8a
Areeba Khokhar <areebak@email.unc.edu>
2022-12-07 15:18:11 -0500

    deleted alot of extra files + started implementing log in

--- client/index.js
+++
@@ -1,9 +0,0 @@
-const username = document.getElementById('username');
-loginUser = (e) => {
-    e.preventDefault();
-    sessionStorage.setItem("username1", username.value);
-    //check if user exists w get
-
-    //if it doesnt exist - create user with post
-    window.location.href = "game.html";
-};
