c2c488d4a190c82b190b774a75572b17a3ec68c5
aw811502 <77753326+aw811502@users.noreply.github.com>
2022-12-07 19:28:43 -0500

    Setup home page and redirect pages

--- client/index.js
+++ client/index.js
@@ -1,5 +1,3 @@
-let USER = ""
-
 createUser = async (e) => {
     try {
         await fetch("http://localhost:9000/app/users/"+document.getElementById('loginUsername').value, {
@@ -26,3 +24,30 @@ findName = async (e) => {
     }
 }
 
+loginUser = async() => {
+    // Get Username in textbox
+    const username = document.getElementById('username').value;
+    console.log(username);
+
+    // If username is an empty string, display to user that name must be filled
+
+    // Make a GET request with user
+
+    // If user does not exist, display to user that account does not exist
+
+
+    // If user exists, get current points of user, pass on username, and redirect to game.html
+    // {
+    //     exists: True/False (false if does not exist)
+    //     user: {
+    //         "username":"Alan",
+    //         "number_attempts":8,
+    //         "number_correct":7,
+    //         "created_at":"2022-12-07T04:46:53.000Z",
+    //         "updated_at":"2022-12-07T06:50:52.000Z"
+    //     } //null if does not exist
+    // }
+
+    // const username = sessionStorage.getItem("username1");
+    // sessionStorage.setItem("username", username);
+}
